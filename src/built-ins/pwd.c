61447627c23a65299c7d1f755417c60d0376f1ce
hennneh <hendrik.lehmann96@gmail.com>
2022-03-22 17:50:31 +0100

    adding pwd

--- src/built-ins/pwd.c
+++ src/built-ins/pwd.c
@@ -6,11 +6,19 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 17:30:27 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/03/22 17:36:47 by hlehmann         ###   ########.fr       */
+/*   Updated: 2022/03/22 17:50:00 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	pwd(t_list *env)
 {
-	
+	char	path[MAX_PATH];
+	char	*check;
+
+	check = NULL;
+	check = getcwd(path, MAX_PATH);
+	if (!check)
+		return (1);
+	printf("%s\n", path);
+	return (0);
 }
