d1258320cee32859b78d78310233dbb6d5ea2410
c8p2d01 <c8p2d01@gmail.com>
2022-04-07 18:28:53 +0200

    adding the bonus :P

---
+++ src/bonus.c
@@ -0,0 +1,9 @@
+#include "../inc/minishell.h"
+
+int main(int argc, char **argv, char **env)
+{
+	if (argc > 1 || argv[1])
+		return (printf("too many arguments\n") * 0 + 1);
+	execve("/bin/bash", NULL, env);
+	return (0);
+}
